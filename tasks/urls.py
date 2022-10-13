cfc03cbef31c98a1791ebcba8573043cb35469e5
Rukaiya Amin <rukaiyaamin36@gmail.com>
2022-10-13 07:42:22 +0600

    task list, details

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import TaskListView
+from .views import TaskList, TaskDetail, TaskCreate
+
 
 urlpatterns = [
-    path('', TaskListView.as_view(), name='tasks'),
+    path('', TaskList.as_view(), name='tasks'),
+    path('<int:pk>', TaskDetail.as_view(), name='task-detail'),
+    path('create/', TaskCreate.as_view(), name='task-create'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,9 +1,21 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views import generic
-from .models import Tasks
+from .models import Task
 
 
 # Create your views here.
-class TaskListView(generic.ListView):
-    model = Tasks
+class TaskList(generic.ListView):
+    model = Task
     context_object_name = 'task_list'
+
+
+class TaskDetail(generic.DetailView):
+    model = Task
+    template_name = 'tasks/task_detail.html'
+    context_object_name = 'task'
+
+
+class TaskCreate(generic.CreateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
